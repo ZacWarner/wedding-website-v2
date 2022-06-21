17405d87f6b797a3b11e2431d455794bbe61a91b
Zac Warner <zektgn@gmail.com>
2022-06-21 16:02:39 -0700

    added some styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,15 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="h-screen w-screen flex flex-col justify-center">
+      <div className='bg-red-200'>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+      <div className='bg-pink-400'>
+        second
+      </div>
     </div>
   )
 }

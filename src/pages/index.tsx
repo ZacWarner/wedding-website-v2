af0b16d46e9fd3994cc34ef4d30097bcf283d038
Zac Warner <zektgn@gmail.com>
2022-06-21 20:58:49 -0700

    minor changes to styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { trpc } from '@/utils/trpc'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
   return (
     <div className="h-screen w-screen flex flex-col justify-center">
       <div className='bg-red-200'>

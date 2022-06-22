c11c076aa1ffecc2c3e815aea7e442ebe4f0d54f
ZacWarner <zektgn@gmail.com>
2022-06-21 21:21:35 -0700

    changed favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,20 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
 
   return (
-    <div className="h-screen w-screen flex flex-col justify-center">
-      <div className='bg-red-200'>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-      <div className='bg-pink-400'>
-        second
+    <div>
+      <Head>
+        <title>ZacAndLisa</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="h-screen w-screen flex flex-col justify-center">
+        <div className='bg-red-200'>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        </div>
+        <div className='bg-pink-400'>
+          second
+        </div>
       </div>
     </div>
   )
